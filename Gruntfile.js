131a4e0c224f7154f9a266ed969d0c3df28f9045
phette23 <phette23@gmail.com>
2014-10-08 15:57:11 -0700

    Gruntfile - make clean rm theme.zip too

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,8 @@ module.exports = function (grunt) {
                     src: [
                         'dist'
                     ]
+                },{
+                    src: [ 'theme.zip' ]
                 }]
             }
         },
